66b41ec7723b0e95884853793175cbd0f83dbbae
FernTheDev <15272073+Fernthedev@users.noreply.github.com>
2020-06-17 22:29:40 -0400

    Fix Builder errors

--- java/terminal/terminal-server/src/main/java/com/github/fernthedev/lightchat/server/terminal/ServerTerminalSettings.java
+++ java/terminal/terminal-server/src/main/java/com/github/fernthedev/lightchat/server/terminal/ServerTerminalSettings.java
@@ -5,18 +5,29 @@ import lombok.*;
 
 @Getter
 @Builder
-@NoArgsConstructor(access = AccessLevel.PRIVATE)
+//@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class ServerTerminalSettings {
 
+    @Builder.Default
     @NonNull
     private ServerSettings serverSettings = new ServerSettings();
 
-
+    @Builder.Default
     protected boolean lightAllowed = true;
+
+    @Builder.Default
     protected boolean allowChangePassword = true;
+
+    @Builder.Default
     protected boolean allowTermPackets = true;
+
+    @Builder.Default
     protected boolean allowPortArgParse = true;
+
+    @Builder.Default
     protected boolean allowDebugArgParse = true;
+
+    @Builder.Default
     protected int port = -1;
 
 }
