74486a876ae42e2741501e13e9b2acedab79b304
FernTheDev <15272073+Fernthedev@users.noreply.github.com>
2023-03-02 21:28:54 -0400

    Expose message as a field

--- java/core/src/main/java/com/github/fernthedev/lightchat/core/packets/PacketProto.kt
+++ java/core/src/main/java/com/github/fernthedev/lightchat/core/packets/PacketProto.kt
@@ -3,12 +3,12 @@ package com.github.fernthedev.lightchat.core.packets
 import com.github.fernthedev.lightchat.core.codecs.AcceptablePacketTypes
 import com.github.fernthedev.lightchat.core.encryption.PacketType
 import com.google.protobuf.Message
+import java.io.Serializable
 
-class PacketProto(override val packetName: String) : AcceptablePacketTypes {
+class PacketProto(@Suppress("MemberVisibilityCanBePrivate") val message: Message) : AcceptablePacketTypes, Serializable {
+    override val packetName: String
+        get() = message.descriptorForType.fullName
 
-    constructor(messageLite: Message) : this(messageLite.descriptorForType.fullName) {
-
-    }
 
     override val packetType: PacketType
         get() = PacketType.PROTOBUF
